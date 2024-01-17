dd07c44a41218547c85e8522f5041f2d59a79330
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-17 20:58:19 +0200

    Update CardStat.tsx

--- components/CardStat.tsx
+++ components/CardStat.tsx
@@ -7,7 +7,7 @@ const CardStat = () => {
         <Image src='https://twemoji.maxcdn.com/2/svg/1f1f7-1f1fc.svg' alt='flag' height={30} width={30} />
         <div className='flex-col'>
             <h4 className='text-sm font-bold'>Rwanda</h4>
-            <p className='text-xs'>bjCSIRT</p>
+            <p className='text-xs lg:text-[10px'>bjCSIRT</p>
         </div>
     </div>  
   )
