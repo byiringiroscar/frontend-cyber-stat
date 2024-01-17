8ab34c6484217902fd3e0e7c40ff8e25a85a7fee
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-17 20:55:44 +0200

    Add card

---
+++ components/CardStat.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Image from 'next/image'
+
+const CardStat = () => {
+  return (
+    <div className='w-full flex justify-between p-3 bg-[#F1F6FB]'>
+        <Image src='https://twemoji.maxcdn.com/2/svg/1f1f7-1f1fc.svg' alt='flag' height={30} width={30} />
+        <div className='flex-col'>
+            <h4 className='text-sm font-bold'>Rwanda</h4>
+            <p className='text-xs'>bjCSIRT</p>
+        </div>
+    </div>  
+  )
+}
+
+export default CardStat
