0bb9b7f765ff13c237a29d734fbe8d510c24960b
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-17 19:24:44 +0200

    Add search btn

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,9 +12,9 @@ const Navbar = () => {
             <Image alt="logo" src={logo} width={180} height={60} />
         </Link>
 
-        <ul className='hidden h-full gap-10 lg:flex'>
+        <ul className='hidden h-full gap-12 lg:flex'>
             {NAV_LINKS.map((link) => (
-                <Link href={link.href} key={link.key} className='text-[#090337] font-medium'>{link.label}</Link>
+                <Link href={link.href} key={link.key} className='text-[#090337] transition-all px-2 py-0.5 cursor-pointer rounded font-medium hover:text-white hover:bg-[#090337]'>{link.label}</Link>
             ))}
         </ul>
         <Image alt="menu" height={32} width={32} src='menu.svg' className="inline-block cursor-pointer lg:hidden" />
