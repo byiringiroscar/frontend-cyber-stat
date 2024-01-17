6a4d8c209fe03a0538541066c01a8fd97048398e
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-17 21:14:58 +0200

    Add footer setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,12 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className='flex flex-col min-h-screen'>
         <Navbar />
-        <main className='relative overflow-hidden'>
+        <main className='relative overflow-hidden flex-grow'>
           {children}
         </main>
-        {/* <Footer /> */}
+        <Footer />
         </body>
     </html>
   )
