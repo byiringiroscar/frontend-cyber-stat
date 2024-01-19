6706550aa2aa0edfcb3a01837860410b6b57c56b
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-19 08:07:51 +0200

    Add single infomation

---
+++ app/home/[id]/page.tsx
@@ -0,0 +1,18 @@
+
+
+
+const InformationDetail = () => {
+    return (
+        <>
+            <div className='flexCenter bg-[#F1F6FB] h-[180px] lg:px-40'>
+               <h4 className="font-bold text-xl text-[#090337]">Information Detail</h4>
+            </div>
+            <div className="max-container padding-container py-8">
+                <h1>hi</h1>
+            </div>
+        </>
+    )
+}
+
+
+export default InformationDetail
