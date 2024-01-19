61493bf90a137ee0f83e08930f5b31baf182819a
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-19 15:02:14 +0200

    Add splash screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
+'use client'
 import type { Metadata } from 'next'
 import Providers from '@/redux/provider'
 import { Inter } from 'next/font/google'
+import SplashScreen from '../components/SplashScreen'
 import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,19 +24,35 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const pathname = usePathname()
+  const isHome = pathname === "/"
+  const [isLoading, setIsLoading] = useState(isHome)
+
+  useEffect(() => {
+    if (isLoading) return
+
+  }, [isLoading])
+
+
   return (
     <Providers>
     <html lang="en">
       <body className='flex flex-col min-h-screen'>
-        <Navbar />
+        {isLoading && isHome ? (
+          <SplashScreen finishLoading={() => setIsLoading(false)} />
+        ): (
+          <>
+          <Navbar />
           <main className='relative overflow-hidden flex-grow'>
               
             {children}
             <ToastContainer />
           </main>
-        <Footer />
+          <Footer />
+          </>
+        )}
         </body>
     </html>
     </Providers>
   )
-}
+} 
---
+++ components/SplashScreen.jsx
@@ -0,0 +1,53 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import anime from "animejs";
+import Image from 'next/image';
+import logo from '../public/logo.jpg'
+
+// type finishLoadingType {
+//   finishLoading: () => void
+// }
+// }
+
+const SplashScreen = ({ finishLoading }) => {
+  const [isMounted, setIsMounted] = useState(false)
+
+  const animate = () => {
+    const loader = anime.timeline({
+      complete: () => finishLoading(),
+    })
+    loader
+    .add({
+      targets: "#logo",
+      duration: 500,
+      // easing: "easeInOutSine",
+      easing: "easeInOutExpo",
+      // opacity: [0, 1],
+      delay: 0,
+      scale: 1,
+    })
+    .add({
+      targets: "#logo",
+      duration: 500,
+      // easing: "easeInOutSine",
+      easing: "easeInOutExpo",
+      // opacity: [0, 1],
+      delay: 0,
+      scale: 1.25,
+    })
+  }
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setIsMounted(true), 10)
+    animate()
+    return ()=> clearTimeout(timeout)
+  })
+  return (
+    <div className='flex h-screen items-center justify-center' isMounted={isMounted}>
+      <Image id='logo' src={logo} alt='logo' width={250} height={200} />
+
+    </div>
+  )
+}
+
+export default SplashScreen
