cb3c91c4ff355d325a576544cfb7d2b5e8789bea
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-19 07:43:51 +0200

    Add href

--- components/CardStat.tsx
+++ components/CardStat.tsx
@@ -1,23 +1,25 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 type InformationType = {
+  id: string,
   flag: string;
   country: string;
   csirt: string;
   country_code: string
 }
 
-const CardStat = ({flag, country, csirt, country_code}: InformationType) => {
+const CardStat = ({id, flag, country, csirt, country_code}: InformationType) => {
   return (
-    <div className='w-full flex justify-between p-3 bg-[#F1F6FB] rounded cursor-pointer'>
+    <Link href='/' className='w-full flex justify-between p-3 bg-[#F1F6FB] rounded cursor-pointer'>
         <Image src={flag} alt='flag' height={30} width={30} />
         <div className='flex-col gap-2'>
             <h4 className='text-sm font-bold'>{country}</h4>
             <p className='text-xs lg:text-[10px'>{csirt}</p>
             <p className='bg-[#4BD1B9] p-[1px] text-white flex justify-center items-center rounded-xl font-bold'>{country_code}</p>
         </div>
-    </div>  
+    </Link>  
   )
 }
 
