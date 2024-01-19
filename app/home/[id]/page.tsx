c611bf7aa8d488f0e040f63289d435956871dfcc
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-19 08:55:37 +0200

    Update page.tsx

--- app/home/[id]/page.tsx
+++ app/home/[id]/page.tsx
@@ -1,14 +1,41 @@
+"use client"
+import { notFound } from 'next/navigation'
+import useSWR from 'swr'
 import Image from "next/image"
+import { usePathname } from "next/navigation";
+
+const fetcher = async(id: any) => {
+    const res = await fetch(`http://127.0.0.1:8000/information/${id}`)
+    const data = await res.json()
+    return data
+}
 
 
 const InformationDetail = () => {
+    const pathname = usePathname();
+    const id = pathname.split('/').slice(-1)[0];
+    const { data, error, isLoading }  = useSWR(id, fetcher)
+    if (isLoading) {
+        return (
+            <div className='flex justify-center items-center pt-8'>Loading....</div>
+          );
+    }
+    if (error) {
+        return (
+            <div className='flex justify-center items-center pt-8'>Error loading data</div>
+          );
+    }
+
+
+
+
     return (
         <>
             <div className='flexCenter bg-[#F1F6FB] h-[180px] lg:px-40'>
-               <h4 className="font-bold text-xl text-[#090337]">Information Detail</h4>
+               <h4 className="font-bold text-xl text-[#090337]">Information {id}</h4>
             </div>
             <div className="max-container padding-container py-8 flex flex-col gap-5 bg-[url('../../../public/footer-bg.jpg') bg-[center_center] bg-no-repeat bg-cover">
-                <h2 className="font-bold text-4xl text-[#090337]">Benin</h2>
+                <h2 className="font-bold text-4xl text-[#090337]">{data.country}</h2>
                 <p>Aliquam hendrerit sollicitudin purus, quis rutrum mi accumsan nec. 
                     Quisque bibendum orci ac nibh facilisis, at malesuada orci congue. 
                     Nullam tempus sollicitudin cursus. Ut et adipiscing erat. Curabitur this is a text link libero tempus congue.
@@ -20,11 +47,11 @@ Donec et justo ante. Vivamus egestas sodales est, eu rhoncus urna semper eu. Cum
 
                 <div className="flex flex-col gap-2">
                     <h4 className="font-bold">CSIRT</h4>
-                    <p>Benin CSIRT</p>
+                    <p>{data.csirt}</p>
                 </div>
                 <div className="flex flex-col gap-2">
                     <h4 className="font-bold">Country Code</h4>
-                    <p>BJ</p>
+                    <p>{data.country_code}</p>
                     </div>
                 <div className="flex flex-col gap-2">
                     <h4 className="font-bold">Country</h4>
@@ -32,7 +59,7 @@ Donec et justo ante. Vivamus egestas sodales est, eu rhoncus urna semper eu. Cum
                     </div>
                 <div className="flex flex-col gap-2">
                     <h4 className="font-bold">Flag</h4>
-                    <Image src="https://twemoji.maxcdn.com/2/svg/1f1e6-1f1e9.svg" alt="flag" height={30} width={30} />
+                    <Image src={data.flag} alt="flag" height={60} width={60} />
                     </div>
             </div>
         </>
