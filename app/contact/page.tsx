4e86944d8d518c69f6f63a056cc4259997609d04
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-21 11:41:50 +0200

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -32,13 +32,13 @@ const Contact = () => {
 
         </div>
         <div className='w-screen pb-5'>
-  <iframe
-    className='w-full h-screen'
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46660.669043361966!2d-76.17429939609431!3d43.03529129888566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d9f3b8019f2991%3A0x58d5929e21a62e5!2sDinosaur%20Bar-B-Que!5e0!3m2!1sen!2sbd!4v1593527523138!5m2!1sen!2sbd"
-    
-    loading="lazy"
-  ></iframe>
-</div>
+        <iframe
+            className='w-full h-screen'
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46660.669043361966!2d-76.17429939609431!3d43.03529129888566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d9f3b8019f2991%3A0x58d5929e21a62e5!2sDinosaur%20Bar-B-Que!5e0!3m2!1sen!2sbd!4v1593527523138!5m2!1sen!2sbd"
+            
+            loading="lazy"
+        ></iframe>
+        </div>
 
     </>
   )
