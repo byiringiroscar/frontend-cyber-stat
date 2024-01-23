fd7c4b82b838833518f4d1f3c982c957205a7770
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-23 16:56:36 +0200

    wip

--- components/UpperBody.tsx
+++ components/UpperBody.tsx
@@ -6,11 +6,11 @@ import Image from 'next/image'
 
 const UpperBody = () => {
   return (
-    <div className='max-container padding-container flex justify-between pb-8 pt-8'>
+    <div className='max-container padding-container flex justify-between pb-8 pt-8 h-full' id='csirt'>
         <div className={styles.item}>
         <h1 className={`${styles.title} text-4xl lg:text-7xl`}>Better Feautures for your security. </h1>
         <p className={`${styles.desc} text-[16px] lg:[24px] font-normal`}>Turning your Idea into reality. we bring together the teams from the global tech industry</p>
-        <Button text='Visual CISRT STAT' url='/' />
+        <Button text='Visual Map' url='#cisrt' />
         </div>
 
 
